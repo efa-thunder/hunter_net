acb8fa9bf70b815ffa8084e9da41f00c25ee14f3
Egor Aref'ev <greg_thunder@mail.ru>
2018-02-15 17:11:10 +0400

    Commit all files

--- app/src/main/java/com/simbirsoft/efa/hunternet/MainActivity.kt
+++ app/src/main/java/com/simbirsoft/efa/hunternet/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.simbirsoft.efa.hunternet
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<View>(R.id.LoginButton) as Button
+        val registerTxt = findViewById<View>(R.id.RegisterTextView) as TextView
+
+        loginBtn.setOnClickListener(View.OnClickListener {
+            view -> login()
+        })
+
+        registerTxt.setOnClickListener(View.OnClickListener {
+            view -> register()
+        })
+
+        }
+
+    private fun login() {
+        val emailTxt = findViewById<View>(R.id.EmailText) as EditText
+        val passwordTxt = findViewById<View>(R.id.PasswordText) as EditText
+
+        var email = emailTxt.text.toString()
+        var password = passwordTxt.text.toString()
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, BaseActivity :: class.java))
+                    Toast.makeText(this, "Successfully logged in...", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "(Un)expected error...", Toast.LENGTH_LONG).show()
+                }
+            })
+        } else {
+            Toast.makeText(this, "Please enter email and/or password...", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun register() {
+        startActivity(Intent(this, RegisterActivity :: class.java))
     }
 }
--- app/src/main/java/com/simbirsoft/efa/hunternet/RegisterActivity.kt
+++ app/src/main/java/com/simbirsoft/efa/hunternet/RegisterActivity.kt
@@ -1,7 +1,51 @@
 package com.simbirsoft.efa.hunternet
 
-/**
- * Created by iClub on 15.02.2018.
- */
-class RegisterActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    private val mAuth = FirebaseAuth.getInstance()
+
+    override fun OnCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val registerBtn = findViewById<View>(R.id.registerButton) as Button
+
+        registerBtn.setOnClickListener(View.OnClickListener {
+            view -> register()
+        })
+    }
+
+    private fun register() {
+        val emailTxt = findViewById<View>(R.id.emailTextInput) as EditText
+        val passwordTxt = findViewById<View>(R.id.passwordTextInput) as EditText
+        val usernameTxt = findViewById<View>(R.id.usernameTextInput) as EditText
+
+        var email = emailTxt.text.toString()
+        var password = passwordTxt.text.toString()
+        var username = usernameTxt.text.toString()
+
+        if (!username.isEmpty() && !password.isEmpty() && !email.isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                } else {
+                    Toast.makeText(this, "(Un)expected error...", Toast.LENGTH_LONG).show()
+                }
+            })
+        } else {
+            Toast.makeText(this, "Please enter username, email and password...", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 }
