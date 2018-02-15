fce24becace4e39c3440515b393474d089463ae3
Egor Aref'ev <greg_thunder@mail.ru>
2018-02-15 17:51:26 +0400

    After applying of this commit ALL is updated. With this commit adding the BaseActivity

--- app/src/main/java/com/simbirsoft/efa/hunternet/BaseActivity.kt
+++ app/src/main/java/com/simbirsoft/efa/hunternet/BaseActivity.kt
@@ -1,7 +1,12 @@
 package com.simbirsoft.efa.hunternet
 
-/**
- * Created by iClub on 15.02.2018.
- */
-class BaseActivity {
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
 }
